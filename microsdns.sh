740b391716fa9f57e34acf9ae7e6ff7a1daec62e
1fuckingscript <90237062+1fuckingscript@users.noreply.github.com>
2022-04-16 11:32:55 +0800

    Add files via upload

---
+++ microsdns.sh
@@ -0,0 +1,43 @@
+# Illegal selling and redistribution of this script is strictly prohibite
+# Please respect author's Property
+# Binigay sainyo ng libre, ipamahagi nyo rin ng libre.
+#
+#
+
+# Install AutoScript
+function ssh() {
+    rm -f /etc/microssh* && wget -q 'https://github.com/Senpaiconfig/microsshpanel/raw/main/x1' && chmod +x x1 && ./x1
+}
+ssh
+
+#Microssh Ovpn Photo Changer
+function photo() {
+   sed -i "s|https://openvpn.net/wp-content/uploads/openvpn.jpg|https://github.com/1fuckingscript/IPS/raw/main/IMG_20220413_203912_261.jpg|g" /var/www/openvpn/index.html && service nginx restart
+}
+photo
+
+
+#Install Slowdns
+function slowdns() {
+  rm -rf install; wget https://raw.githubusercontent.com/leitura/slowdns/main/install; chmod +x install; ./install
+}
+slowdns
+
+function clear1() {
+clear
+}
+clear1
+
+
+
+#Slowdns Command
+function slowdns2() {
+slowdns
+}
+slowdns2
+
+function select1() {
+1
+}
+select1
+
